abb27ca2f08a90461d05ccd8f1572801d189b935
timwis <tim@timwis.com>
2015-11-20 07:26:47 -0500

    Add test for create() #3

--- tests/test_soda.py
+++ tests/test_soda.py
@@ -124,14 +124,47 @@ def test_delete():
         assert isinstance(e, requests_mock.exceptions.NoMockAddress)
     finally:
         client.close()
+        
+def test_create():
+    mock_adapter = {}
+    mock_adapter["prefix"] = PREFIX
+    adapter = requests_mock.Adapter()
+    mock_adapter["adapter"] = adapter
+    client = Socrata(DOMAIN, APPTOKEN, username=USERNAME, password=PASSWORD,
+                     session_adapter=mock_adapter)
+    
+    response_data = "create_foobar.txt"
+    resource = "/api/views.json"
+    set_up_mock(adapter, "POST", response_data, 200, resource=resource)
+    
+    columns = [
+        {"fieldName": "foo", "name": "Foo", "dataTypeName": "text"},
+        {"fieldName": "bar", "name": "Bar", "dataTypeName": "number"}
+    ]
+    response = client.create("Foo Bar", description="test dataset", 
+                  columns=columns, row_identifier="bar")
+    
+    request = adapter.request_history[0]
+    request_payload = json.loads(request.text) # can't figure out how to use .json
+    
+    # Test request payload
+    for dataset_key in ["name", "description", "columns"]:
+        assert dataset_key in request_payload
 
+    for column_key in ["fieldName", "name", "dataTypeName"]:
+        assert column_key in request_payload["columns"][0]
+    
+    # Test response
+    assert isinstance(response, dict)
+    assert len(response.get("id")) == 9
+    client.close()
 
 def set_up_mock(adapter, method, response, response_code,
-                reason="OK", auth=None):
+                reason="OK", auth=None, resource=PATH):
     path = os.path.join(TEST_DATA_PATH, response)
     with open(path, "rb") as f:
         body = json.load(f)
-    uri = "{0}://{1}{2}".format(PREFIX, DOMAIN, PATH)
+    uri = "{0}://{1}{2}".format(PREFIX, DOMAIN, resource)
     headers = {
         "content-type": "application/json; charset=utf-8"
     }
