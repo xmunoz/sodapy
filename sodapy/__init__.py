90f3440525280c76ce792bb06413623ff872f5d7
matt ritter <matthew.w.ritter@gmail.com>
2018-02-07 21:43:32 -0500

    Fixed issue with domain regex

--- sodapy/__init__.py
+++ sodapy/__init__.py
@@ -48,7 +48,12 @@ class Socrata(object):
         '''
         if not domain:
             raise Exception("A domain is required.")
-        self.domain = re.sub(r'http[s]+[:\/\/]+', '', domain)
+        core_url = re.compile('^(http:\/\/){0,1}(https:\/\/){0,1}([\w|\.]+)\/{0,1}.*?$')
+        try:
+            self.domain = re.search(core_url, domain)[3]
+        except IndexError:
+            raise Exception("domain should not contain slashes, and should look something like 'opendata.socrata.com'")
+
 
         # set up the session with proper authentication crendentials
         self.session = requests.Session()
