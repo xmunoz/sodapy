b79cbbfacfc6ce5a442b4fcc2878e3402b51024e
Cristina <hi@xmunoz.com>
2020-04-22 10:02:38 -0700

    Add test coverage for utils (#73)
    * Add test coverage for utils

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,6 +1,7 @@
 import sodapy.utils as utils
 import pytest
 import requests
+import requests_mock
 
 
 @pytest.mark.parametrize(
@@ -26,3 +27,81 @@ def test_raise_for_status(status_code, status_type, reason, raises_exception):
             utils.raise_for_status(response)
     else:
         utils.raise_for_status(response)
+
+
+@pytest.mark.parametrize(
+    ("elems", "result"),
+    [
+        ({}, {}),
+        ({"a": 1, "b": None, "c": "d"}, {"a": 1, "c": "d"}),
+        ({"s": "", "c": 0}, {"s": "", "c": 0}),
+    ],
+)
+def test_clear_empty_values(elems, result):
+    assert utils.clear_empty_values(elems) == result
+
+
+def test_format_old_api_request_exception():
+    with pytest.raises(Exception):
+        utils.format_old_api_request()
+
+
+@pytest.mark.parametrize(
+    ("dataid", "content_type", "path"),
+    [
+        ("abcd", None, "/api/views/abcd"),
+        ("abcd", "json", "/api/views/abcd.json"),
+        (None, "json", "/api/views.json"),
+    ],
+)
+def test_format_old_api_request(dataid, content_type, path):
+    assert (
+        utils.format_old_api_request(dataid=dataid, content_type=content_type) == path
+    )
+
+
+@pytest.mark.parametrize(
+    ("dataid", "row_id", "content_type", "path"),
+    [
+        ("abcd", None, "json", "/resource/abcd.json"),
+        ("abcd", 123, "json", "/resource/abcd/123.json"),
+    ],
+)
+def test_format_new_api_request(dataid, row_id, content_type, path):
+    assert (
+        utils.format_new_api_request(
+            dataid=dataid, row_id=row_id, content_type=content_type
+        )
+        == path
+    )
+
+
+def test_format_new_api_request_exception():
+    with pytest.raises(Exception):
+        utils.format_new_api_request()
+
+
+@pytest.mark.parametrize(
+    ("username", "password", "token"),
+    [("me", None, "123456"), (None, "pass", "123456"), ("me", "pass", "123456")],
+)
+def test_authentication_validation_exceptions(username, password, token):
+    with pytest.raises(Exception):
+        utils.authentication_validation(username, password, token)
+
+
+@pytest.mark.parametrize(
+    ("username", "password", "token"), [("me", "pass", None), (None, None, "93738")],
+)
+def test_authentication_validation(username, password, token):
+    utils.authentication_validation(username, password, token)
+
+
+def test_download_file(tmp_path):
+    p = tmp_path / "myfile.txt"
+    url = "http://fileserver.dev/file"
+    text = "the response data"
+    with requests_mock.Mocker() as m:
+        m.get(url, text=text)
+        utils.download_file(url, str(p))
+    assert p.read_text() == text
